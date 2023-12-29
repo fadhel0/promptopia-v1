cd5d8dada198628ef5263f4d4eced08ac05f1556
fadhel0 <fadhel.gaaloul@gmail.com>
2023-12-29 11:52:15 +0100

    add user model, the database and configue nextauth

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,9 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 const Nav = () => {
-  const isUserLoggedIn = true;
+  //const isUserLoggedIn = true;
+
+  const { data: session } = useSession;
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
@@ -36,7 +38,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -76,7 +78,7 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
